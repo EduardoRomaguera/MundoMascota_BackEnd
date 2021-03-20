f70f85a516220312c058f713039680fd7fe49030
EduardoRomaguera <eromagueras@ucenfotec.ac.cr>
2021-03-19 20:58:12 -0600

    Clase 3-19-2021
    Primer colección en archivo usuarios.models.js

---
+++ models/usuarios.model.js
@@ -0,0 +1,15 @@
+'use strict';
+const mongoose = require('mongoose');
+
+const schema_usuario = new mongoose.Schema({
+correo : {type: String, required: true, unique: true},
+nombre : {type: String, required: true, unique: false},
+nacimiento : {type: Date, required: true, unique: false},
+sexo : {type: String, required: true, unique: false},
+tipo : {type: String, required: true, unique: false},
+contrasenna : {type: String, required: false, unique: false},
+estado : {type: String, required: true, unique: false}
+
+});
+
+module.exports = mongoose.model('Usuario', schema_usuario, 'usuarios');
