7fc42f5ee43cbb17dcab27f4ac8b76a851578e07
alejandro290119 <80568744+alejandro290119@users.noreply.github.com>
2021-04-22 18:23:51 -0500

    cliente route update
    cliente route update

--- models/cliente.model.js
+++ models/cliente.model.js
@@ -6,11 +6,15 @@ const schema_cliente = new mongoose.Schema({
     apellido1: { type: String, required: true, unique: false },
     apellido2: { type: String, required: true, unique: false },
     correo: { type: String, required: true, unique: true },
+    tipoID: { type: String, required: true, unique: false },
     identificacion: { type: String, required: true, unique: true },
-    tipo: { type: String, required: true, unique: false },
     estado: { type: String, required: true, unique: false },
+    tipo: { type: String, required: true, unique: false },
     nacimiento: { type: Date, required: true, unique: false },
-    ubicacion: { type: String, reqired: true, unique: false },
+    provincia: { type: String, reqired: true, unique: false },
+    canton: { type: String, reqired: true, unique: false },
+    distrito: { type: String, reqired: true, unique: false },
+    sennas: { type: String, reqired: true, unique: false },
 
 });
 
--- routes/clientes.route.js
+++ routes/clientes.route.js
@@ -1,34 +1,37 @@
 'use strict';
 const express = require('express');
-const Cliente = require('../models/cliente.model');
+const Usuario = require('../models/cliente.model');
+const mailTemplate = require('../templates/registro-cliente');
 const router = express.Router();
 
-
-router.post('/registrar-usuario', (req, res) => {
-    let nuevoCliente = new Usuario({
-        correo: req.body.correo,
+router.post('/registrar-usuario-cliente', (req, res) => {
+    let nuevoUsuarioCliente = new Usuario({
         nombre: req.body.nombre,
+        apellido1: req.body.apellido1,
+        apellido2: req.body.apellido2,
+        tipoID: req.body.tipoId,
+        identificacion: req.body.identificacion,
         nacimiento: req.body.nacimiento,
-        sexo: req.body.sexo,
-        tipo: req.body.tipo,
-        contrasenna: req.body.contrasenna,
-        estado: req.body.estado
+        correo: req.body.correo,
+        provincia: req.body.provincia,
+        canton: req.body.canton,
+        distrito: req.body.distrito,
+        sennas: req.body.sennas,
+        tipo: 'Cliente',
+        estado: 'Activo'
     });
-
-
-
-    nuevoCliente.save((error) => {
+    nuevoUsuarioCliente.save((error) => {
         if (error) {
             res.json({
-                msj: 'Ocurrió un error al registrar el Cliente',
+                msj: 'Ocurrió un error al registrar el usuario',
                 error
             });
         } else {
-            res.json({
-                msj: 'El Cliente se registró adecuadamente'
-            });
+            mailTemplate.enviar_mail(req.body.nombre, req.body.correo);
+            // res.json({
+            //     msj: 'El usuario se registró adecuadamente'
+            // });
         }
-
     });
 
 });
