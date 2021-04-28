d4105135dcc3e38c78c19b91d2b5ccd47a036fd7
Kathy <ksegurag1@ucenfotec.ac.cr>
2021-04-27 23:29:02 -0600

    se agrega cambio a catalogo-servicios

--- models/serviciosCatalogo.model.js
+++ models/serviciosCatalogo.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const schema_servicioCatalogo = mongoose.Schema({
     nombre: { type: String, required: true, unique: false },
-    descripcion: { type: String, required: true, unique: false }
+    descripcion: { type: String, required: true, unique: false },
+    estado: { type: String, required: true, unique: false }
 });
 module.exports = mongoose.model('ServicioCatalogo', schema_servicioCatalogo, 'serviciosCatalogo');
