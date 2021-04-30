4131ce00059c5bbf8e2baf74495337ba5c2a61f7
Kathy <ksegurag1@ucenfotec.ac.cr>
2021-04-30 00:58:40 -0600

    se le agrega el telefono a la mascota

--- models/mascotas.model.js
+++ models/mascotas.model.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const schema_mascota = mongoose.Schema({
     correoUsuario: { type: String, requiered: true, unique: true },
     nombre: { type: String, required: true, unique: false },
+    telefono: { type: String, required: true, unique: false },
     especie: { type: String, required: true, unique: false },
     raza: { type: String, required: true, unique: false },
     estado: { type: String, required: true, unique: false },
