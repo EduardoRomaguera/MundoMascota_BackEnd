c44d719c0bf4c93ddaa5aade9d3818d78820e084
Kathy <ksegurag1@ucenfotec.ac.cr>
2021-04-30 19:18:57 -0600

    listar mascotas

--- routes/mascotas.route.js
+++ routes/mascotas.route.js
@@ -33,4 +33,54 @@ router.post('/registrar-mascota', (req, res) => {
         }
     });
 });
+
+router.get('/listar-mascotas', (req, res) => {
+    Mascota.find((error, mascotas) => {
+        if (error) {
+            res.json({
+                msj: 'Ocurrió un error al listar las mascotas',
+                error
+            });
+        } else {
+            res.json({
+                mascotas
+            });
+        }
+    });
+});
+
+router.put('/modificar-tarjeta', (req, res) => {
+    Tarjeta.updateOne({ _id: req.body._id }, {
+        $set: req.body
+    }, (err, info) => {
+        if (err) {
+            res.json({
+                msj: 'No se pudo modificar la tarjeta',
+                err
+
+            });
+        } else {
+            res.json({
+                msj: 'La tarjeta se guardó correctamente'
+            });
+        }
+    });
+
+});
+
+router.delete('/eliminar-tarjeta', (req, res) => {
+    Tarjeta.deleteOne({ _id: req.body._id }, (error) => {
+        if (error) {
+            res.json({
+                msj: 'Ocurrió un error al eliminar la tarjeta',
+                error
+            });
+        } else {
+            res.json({
+                msj: 'La tarjeta ha ha sido eliminado'
+            });
+        }
+    });
+});
+
 module.exports = router;
